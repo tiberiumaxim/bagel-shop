909872e060e497c52f09a3445b07d7e93d8a149b
Tiberiu Maxim <tiberiu@itgambit.com>
2016-07-29 23:17:41 +0300

    raw menu items sections + animations

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import { ENV } from './config';
 
 require('expose?$!expose?jQuery!jquery');
 require('bootstrap-loader');
+require('animate.css/animate.css');
 require('./../style/main.scss');
 if (ENV === 'development') {
 	require('./../index.html');
